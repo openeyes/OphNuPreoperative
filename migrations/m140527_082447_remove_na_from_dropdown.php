696592dbc3f1319dc97dccb16a20c28b6b2a2419
Mark Wadham <m@rkw.me>
2014-07-25 12:34:23 +0000

    edge case migration bugfix

--- migrations/m140527_082447_remove_na_from_dropdown.php
+++ migrations/m140527_082447_remove_na_from_dropdown.php
@@ -4,7 +4,10 @@ class m140527_082447_remove_na_from_dropdown extends CDbMigration
 {
 	public function up()
 	{
-		$this->delete('ophnupreoperative_preoperative_wristband',"name = 'N/A'");
+		$id = $this->dbConnection->createCommand()->select("id")->from("ophnupreoperative_preoperative_wristband")->where("name = 'N/A'")->queryScalar();
+
+		$this->delete('et_ophnupreoperative_preoperative_wristband_assignment',"ophnupreoperative_preoperative_wristband_id = $id");
+		$this->delete('ophnupreoperative_preoperative_wristband',"id = $id");
 	}
 
 	public function down()
