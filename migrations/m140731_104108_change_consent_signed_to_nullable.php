a27118a7ce4ae6dc0b767a48931028f19dc0e3b9
Richard Willis <willis.rh@gmail.com>
2014-07-31 12:48:48 +0100

    [ORB-299] Change version table

--- migrations/m140731_104108_change_consent_signed_to_nullable.php
+++ migrations/m140731_104108_change_consent_signed_to_nullable.php
@@ -5,12 +5,16 @@ class m140731_104108_change_consent_signed_to_nullable extends CDbMigration
 	public function up()
 	{
 		$this->alterColumn('et_ophnupreoperative_preoperative', 'consent_signed', 'tinyint(1) unsigned null');
+		$this->alterColumn('et_ophnupreoperative_preoperative_version', 'consent_signed', 'tinyint(1) unsigned null');
 		$this->refreshTableSchema('et_ophnupreoperative_preoperative');
+		$this->refreshTableSchema('et_ophnupreoperative_preoperative_version');
 	}
 
 	public function down()
 	{
 		$this->alterColumn('et_ophnupreoperative_preoperative', 'consent_signed', 'tinyint(1) unsigned not null');
+		$this->alterColumn('et_ophnupreoperative_preoperative_version', 'consent_signed', 'tinyint(1) unsigned not null');
 		$this->refreshTableSchema('et_ophnupreoperative_preoperative');
+		$this->refreshTableSchema('et_ophnupreoperative_preoperative_version');
 	}
 }
