1656ca0cc0ba5d6bba55622231444d8001a942e4
Mark Wadham <m@rkw.me>
2014-08-01 14:11:04 +0000

    fixed crash

--- views/default/form_Element_OphNuPreoperative_PreoperativeAssessment.php
+++ views/default/form_Element_OphNuPreoperative_PreoperativeAssessment.php
@@ -46,7 +46,7 @@
 		<?php echo $form->radioBoolean($element, 'surgical_site_verified', array('class' => 'linked-fields', 'data-linked-fields' => 'site_id', 'data-linked-values' => 'Yes'), array('label' => 3, 'field' => 4))?>
 		<?php echo $form->radioButtons($element, 'site_id', 'OphNuPreoperative_PreoperativeAssessment_Site',null,false,!$element->surgical_site_verified==1,false,false,array(),array('label' => 3, 'field' => 4))?>
 		<?php echo $form->radioButtons($element, 'iol_verified_id', 'OphNuPreoperative_PreoperativeAssessment_IolVerified', null, false, false, false, false, array('class' => 'linked-fields', 'data-linked-fields' => 'iol_side_id', 'data-linked-values' => 'Yes'), array('label' => 3, 'field' => 4))?>
-		<?php echo $form->radioButtons($element, 'iol_side_id', 'eye', null, false, !$element->iol_verified_id || $element->iol_verified->name != 'Yes', false, false, array(), array('label' => 3, 'field' => 4))?>
+		<?php echo $form->radioButtons($element, 'iol_side_id', 'Eye', null, false, !$element->iol_verified_id || $element->iol_verified->name != 'Yes', false, false, array(), array('label' => 3, 'field' => 4))?>
 		<div id="div_Element_OphNuPreoperative_PreoperativeAssessment_right_iol_type_id" class="row field-row"<?php if (!$element->iol_side) {?> style="display: none"<?php }?>>
 			<div class="large-3 column">
 				<label for="Element_OphCiPatientadmission_NpoStatus_right_iol_type_id">
