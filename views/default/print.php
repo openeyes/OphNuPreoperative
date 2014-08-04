fa58f463dcef80ef634c5b1b1ee5f5d86f5eab3c
Richard Willis <willis.rh@gmail.com>
2014-08-04 11:48:43 +0100

    [ORB-323] Fixed print and removed redundant view files

--- views/default/print.php
+++ views/default/print.php
@@ -18,7 +18,7 @@
  */
 ?>
 
-<?php $this->renderDefaultElements($this->action->id)?>
-
-
-
+<?php $this->renderPartial('//print/event', array(
+	'hide_modified' => @$hide_modified,
+	'hide_created' => @$hide_created
+));?>
